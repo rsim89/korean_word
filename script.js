5d410d23c71926a34432c6e1db99bfd1d487fdb0
rsim89 <118846661+rsim89@users.noreply.github.com>
2024-10-20 22:17:07 -0400

    Update script.js

--- script.js
+++ script.js
@@ -120,7 +120,7 @@ function selectCard(card) {
 }
 
 function playSound(soundFile) {
-    const audio = new Audio(`https://rsim89.github.io/korean_word/audiofiles/${soundFile}`);
+    const audio = new Audio(`https://rsim89.github.io/korean_word/audiofiles/KORE121/${soundFile}`);
     audio.play();
 }
 
